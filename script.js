00da9b6ada95c536639552d62bf257f8ffa46501
Jean Lucas <j3anlucas010@gmail.com>
2025-03-19 11:28:29 -0300

    Cria a span para adicionar o R$ formatado, Formata o valor e remove o R$ que será exibido pela small com um estilo customizado, Limpa o conteúdo do elemento, Adiciona o símbolo da moeda e o valor total formatado

--- script.js
+++ script.js
@@ -115,7 +115,7 @@ function updateTotals(){
       const itemAmount=items[item].querySelector('.expense-amount')
 
       //Remove caracteres não númericos e substitui a vírgula pelo ponto.
-      let value=itemAmount.textContent.replace(/[^\d]/g, "").replace(',',".")
+      let value=itemAmount.textContent.replace(/[^\d,]/g, "").replace(',',".")
 
       //Converte o valor para float
       value=parseFloat(value)
@@ -129,8 +129,19 @@ function updateTotals(){
       total+=Number(value)
       
     }
-    expensesTotal.textContent=total
 
+    //Cria a span para adicionar o R$ formatado.
+    const symbolBRL=document.createElement('small')
+    symbolBRL.textContent="R$"
+
+    //Formata o valor e remove o R$ que será exibido pela small com um estilo customizado.
+    total=fomatCurrencyBRL(total).toUpperCase().replace("R$", "")
+
+    //Limpa o conteúdo do elemento.
+    expensesTotal.innerHTML=''
+
+    //Adiciona o símbolo da moeda e o valor total formatado.
+    expensesTotal.append(symbolBRL,total)
   } catch (error) {
     console.log(error)
     alert("Não foi possível atualizar os totais. ")
