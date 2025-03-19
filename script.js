40dc518c86b526732b0ce5bfaa7d6d13621df533
Jean Lucas <j3anlucas010@gmail.com>
2025-03-19 11:00:04 -0300

    Remove caracteres não númericos e substitui a vírgula pelo ponto, Converte o valor para float, Verifica se é um número válido, Incrementa o valor Total.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const form = document.querySelector("form");
 
 //Seleciona os elementos da lista.
 const expenseList = document.querySelector("ul");
+const expensesTotal=document.querySelector('aside header h2')
 const expenseQuantity=document.querySelector('aside header p span')
 
 //Tirando as Letras Do input,É deixando apenas números.
@@ -108,12 +109,27 @@ function updateTotals(){
 
     //Variável para incrementar o total.
     let total=0
+
     //Percorre cada item (li) da lista (ul)
     for(let item= 0;item <items.length;item ++){
       const itemAmount=items[item].querySelector('.expense-amount')
 
+      //Remove caracteres não númericos e substitui a vírgula pelo ponto.
+      let value=itemAmount.textContent.replace(/[^\d]/g, "").replace(',',".")
+
+      //Converte o valor para float
+      value=parseFloat(value)
+
+      //Verifica se é um número válido 
+      if(isNaN(value)){
+        return alert('Não foi possível atualizar os totais.O valor não parecer ser um número.')
+      }
+
+      //Incrementa o valor Total.
+      total+=Number(value)
       
     }
+    expensesTotal.textContent=total
 
   } catch (error) {
     console.log(error)
